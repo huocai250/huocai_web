1ea7d42376e36728c05ab82d2ae53f01d76f7f60
huocai <165896141+huocai250@users.noreply.github.com>
2026-08-19 17:06:29 +0800

    Enable Giscus comments and reactions

--- blog-config.js
+++ blog-config.js
@@ -1,2 +1,2 @@
-window.BLOG_CONFIG={giscus:{repo:'huocai250/huocai_web',repoId:'',category:'General',categoryId:''}};
+window.BLOG_CONFIG={giscus:{repo:'huocai250/huocai_web',repoId:'R_kgDOT9P3Mg',category:'General',categoryId:'DIC_kwDOT9P3Ms4DDtWL'}};
 
